60843a08c03a6c3dd6514110810740f63daafe41
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-11-30 16:20:12 +0100

    add form

--- src/components/CreateNewUser.tsx
+++ src/components/CreateNewUser.tsx
@@ -1,9 +1,30 @@
+import React, { useState } from 'react'
 import AddIcon from '../icons/AddIcon'
 
 export default function CreateNewUser() {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+	}
+
 	return (
-		<button className='w-14 h-14 bg-slate-100 rounded flex items-center justify-center'>
-			<AddIcon />
-		</button>
+		<>
+			{isOpen ? (
+				<form
+					onSubmit={handleSubmit}
+					className='bg-slate-100 flex flex-col justify-center items-center'>
+					<input type='text' placeholder='Name...' />
+					<input type='text' placeholder='Email' />
+					<button type='submit'>Create User</button>
+				</form>
+			) : (
+				<button
+					onClick={() => setIsOpen(!isOpen)}
+					className='w-14 h-14 bg-slate-100 rounded flex items-center justify-center'>
+					<AddIcon />
+				</button>
+			)}
+		</>
 	)
 }
