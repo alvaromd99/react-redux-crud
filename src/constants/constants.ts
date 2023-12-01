ef14f049e43ce3823a68600d260027828f3fb8c5
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-12-01 15:54:08 +0100

    put images on constants file

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -23,13 +23,18 @@ const defaultUsers = [
 	},
 ]
 
+export const images = [
+	'https://images.unsplash.com/photo-1701273973387-8abff988bb88?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1511367461989-f85a21fda167?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1579783483458-83d02161294e?q=80&w=1397&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+]
+
 // Immediately-invoked function (IIFE)
 export const initialState: UserWithId[] = (() => {
 	const persistedState = localStorage.getItem('reduxState')
 
 	return persistedState ? JSON.parse(persistedState).users : defaultUsers
 })()
-
-/* const randomPhoto =
-	'https://unsplash.com/es/fotos/hombre-con-chaqueta-negra-y-gorra-marron-YUu9UAcOKZ4'
- */
